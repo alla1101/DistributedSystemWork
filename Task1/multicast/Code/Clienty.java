5a150503e61c003438d90c357c3d4493bd5a2f0b
Alla Barakat <a2la1101@gmail.com>
2019-11-16 18:18:06 +0200

    completed multicast,therefore completed task 1

---
+++ Task1/multicast/Code/Clienty.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+//UDPMulticastClient
+public class Clienty implements Runnable {
+
+    public static void main(String[] args) {
+       Thread t=new Thread(new Clienty());
+       t.start();
+    }
+ 
+    public void receiveUDPMessage(String ip, int port) throws
+          IOException {
+       byte[] buffer=new byte[1024];
+       MulticastSocket socket=new MulticastSocket(port);
+       InetAddress group=InetAddress.getByName(ip);
+       socket.joinGroup(group);
+       while(true){
+          System.out.println("Waiting for multicast message...");
+          DatagramPacket packet=new DatagramPacket(buffer,
+             buffer.length);
+          socket.receive(packet);
+          String msg=new String(packet.getData(),
+          packet.getOffset(),packet.getLength());
+          System.out.println("[Multicast UDP message received]"+msg);
+          if("OK".equals(msg)) {
+             System.out.println("No more message. Exiting : "+msg);
+             break;
+          }
+       }
+       socket.leaveGroup(group);
+       socket.close();
+    }
+ 
+    @Override
+    public void run(){
+         try {
+             receiveUDPMessage("224.0.0.0", 4321);
+         }catch(IOException ex){
+             ex.printStackTrace();
+         }
+     }
+ }
